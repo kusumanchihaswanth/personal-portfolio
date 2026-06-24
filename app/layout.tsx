04bd12adf8c5a4ac9c69c298d9bc0b8ffc155320
kusumanchihaswanth <kusumanchihaswanth@gmail.com>
2026-06-24 20:51:26 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,8 @@ import './global.css'
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
-import { Navbar } from './components/nav'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import Footer from './components/footer'
 import { baseUrl } from './sitemap'
 
 export const metadata: Metadata = {
@@ -47,16 +45,14 @@ export default function RootLayout({
     <html
       lang="en"
       className={cx(
-        'text-black bg-white dark:text-white dark:bg-black',
+        'bg-neutral-900 text-neutral-100',
          GeistSans.variable,
          GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
-          <Navbar />
+      <body className="antialiased m-0 p-0 overflow-x-hidden bg-neutral-900">
+        <main className="w-full min-h-screen">
           {children}
-          <Footer />
           <Analytics />
           <SpeedInsights />
         </main>
